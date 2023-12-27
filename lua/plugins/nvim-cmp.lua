0fec4bdd47bb24bfa644de749bf44f124f12c93c
Fraz Mahmud <fraz501@hotmail.com>
2023-12-26 22:57:45 -0500

    removed the ability from auto completion to manually select an item

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -21,7 +21,7 @@ return {
 
     cmp.setup({
       completion = {
-        completeopt = "menu,menuone,preview,noinsert,noselect",
+        completeopt = "menu,menuone,preview,noinsert",
       },
       snippet = { -- configure how nvim-cmp interacts with snippet engine
         expand = function(args)
